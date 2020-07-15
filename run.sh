4d76a7adbdbd541f1c17617ec28a85f54c479822
ruinanzhang <rz2363@columbia.edu>
2020-07-15 03:16:45 -0400

    added my docker-compose file and run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Create new confluent vm container..."
+docker-machine create --driver virtualbox --virtualbox-memory 6
+
+echo "Config terminal window to attach it to new docker machine..."
+eval $(docker-machine env confluent)
